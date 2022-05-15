2927aa6b14db98e3e678ec7cec927b6273bf037b
松本愛可 <matsumotomanaka@matsumoikanoAir.sensyu.ne.jp>
2022-05-15 10:16:57 +0900

    Initial Commit

---
+++ sample.rb
@@ -0,0 +1,12 @@
+class Product
+  DEFAULT_PRICE = 0
+  def self.default_price
+    DEFAULT_PRICE
+  end
+  def default_price
+    DEFAULT_PRICE
+  end
+end
+Product.default_price
+product = Product.new
+product.price
